4cdf67bd9f861502a630f7f7a7ff918431b5cc4e
vcodes1 <vinaycodes1@gmail.com>
2023-02-21 18:05:28 +0530

    get and display Movies from server

--- client/src/component/MovieList/MovieList.js
+++ client/src/component/MovieList/MovieList.js
@@ -1,35 +1,39 @@
 import Movie from '../Movie/Movie.js';
+import axios from 'axios';
+import {useEffect, useState} from 'react'
 
-const harryPotterFirst = {
-  title: "Harry Potter and the Sorcers stone",
-  image: "https://m.media-amazon.com/images/I/51HSkTKlauL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
-  rating: 5
-};
+// const harryPotterFirst = {
+//   title: "Harry Potter and the Sorcers stone",
+//   image: "https://m.media-amazon.com/images/I/51HSkTKlauL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+//   rating: 5
+// };
 
-const sherlockHolmes = {
-  title: "Sherlock Holmes - the pink case",
-  image: "https://m.media-amazon.com/images/I/51ZqLGyqiGL._SY346_.jpg",
-  rating: 4.5
-};
+// const sherlockHolmes = {
+//   title: "Sherlock Holmes - the pink case",
+//   image: "https://m.media-amazon.com/images/I/51ZqLGyqiGL._SY346_.jpg",
+//   rating: 4.5
+// };
 
-const ironman = {
-  title: "Iron man vs Batman",
-  image: "https://m.media-amazon.com/images/I/51uVGofXjIL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
-  rating: 4
-};
+// const ironman = {
+//   title: "Iron man vs Batman",
+//   image: "https://m.media-amazon.com/images/I/51uVGofXjIL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+//   rating: 4
+// };
 
-const spiderman = {
-  title: "Spider man in the multiverse",
-  image: "https://m.media-amazon.com/images/I/51qWmQzgecL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-  rating: 5
-};
+// const spiderman = {
+//   title: "Spider man in the multiverse",
+//   image: "https://m.media-amazon.com/images/I/51qWmQzgecL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+//   rating: 5
+// };
 
-const movies = [
-  harryPotterFirst,
-  sherlockHolmes,
-  ironman,
-  spiderman
-];
+// const movies = [
+//   harryPotterFirst,
+//   sherlockHolmes,
+//   ironman,
+//   spiderman
+// ];
+
+
 
 const showMovies = (movie) => {
   return(<div>
@@ -38,11 +42,18 @@ const showMovies = (movie) => {
 }
 
 const MovieList = ()=>{
+  const [movies,setMovies] = useState([]);
+  useEffect( ()=>{
+    const fetchData = async () => {
+    const req = await axios.get(`http://localhost:9000/movies`);
+    setMovies(req.data)};
+    fetchData();
+  }, [])
   return (
     <div>
     {movies.map(showMovies)}
     </div>
   );
-};
+}; 
 
 export default MovieList;
